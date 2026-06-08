a03f9578bd7822610a2555bc029a2b027e8b6b60
mayankmalik263 <mayankmalik263@gmail.com>
2026-06-08 17:24:58 +0530

    replace tkinter window setup with user input prompt in input.py

--- input.py
+++ input.py
@@ -1,7 +1,1 @@
-import tkinter as tk
-
-root = tk.Tk() # creates a window program that automatically appears and then disappears. 
-# Root -> variable name of the window Tk() -> class in tkinter class that initialize the GUI System and then return a window object
-
-root.title("First Input Window")
-root.mainloop()
+idea = input("Enter your business Idea: ")
