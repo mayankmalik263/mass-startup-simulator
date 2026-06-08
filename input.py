fbc67b750c9b68fa3ac9d31e08f026ca46ff761f
mayankmalik263 <mayankmalik263@gmail.com>
2026-06-08 17:15:38 +0530

    add initial tkinter window setup in input.py

---
+++ input.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk() # creates a window program that automatically appears and then disappears. 
+# Root -> variable name of the window Tk() -> class in tkinter class that initialize the GUI System and then return a window object
+
+root.title("First Input Window")
+root.mainloop()
