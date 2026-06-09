b837b5334bea31ccdfb0bdfdb4bde52ad5282d10
mayankmalik263 <mayankmalik263@gmail.com>
2026-06-09 22:51:31 +0530

    add project_state.py to manage application state and update input.py to store startup idea

--- input.py
+++ input.py
@@ -1,1 +1,9 @@
 idea = input("Enter your business Idea: ")
+
+from project_state import state
+
+idea = input("Enter your business Idea: ")
+
+state["startup_idea"] = idea
+
+print(state)
---
+++ project_state.py
@@ -0,0 +1,8 @@
+state = {
+    "startup_idea": "",
+    "target_customer": "",
+    "messages": [],
+    "decisions": [],
+    "final_report": ""
+}
+
