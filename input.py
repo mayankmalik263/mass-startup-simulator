70d2038bcc8403fece2f4f36b56d783606190423
mayankmalik263 <mayankmalik263@gmail.com>
2026-06-12 20:53:15 +0530

    remove input.py and project_state.py to simplify application structure

--- input.py
+++
@@ -1,9 +0,0 @@
-idea = input("Enter your business Idea: ")
-
-from project_state import state
-
-idea = input("Enter your business Idea: ")
-
-state["startup_idea"] = idea
-
-print(state)
--- project_state.py
+++
@@ -1,8 +0,0 @@
-state = {
-    "startup_idea": "",
-    "target_customer": "",
-    "messages": [],
-    "decisions": [],
-    "final_report": ""
-}
-
